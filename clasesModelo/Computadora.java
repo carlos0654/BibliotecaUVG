9e6ac8a4b8bf06452f32dd4c9ce3cc0807ea5f5b
Jimaa07 <mor22781@uvg.edu.gt>
2025-08-26 08:40:00 -0600

    .

---
+++ clasesModelo/Computadora.java
@@ -0,0 +1,20 @@
+package clasesModelo;
+
+public class Computadora {
+    
+
+private int noSerie;
+private String marca;
+private boolean enUso;
+
+public Computadora (int noSerie,  String marca ) {
+}
+public String getNoSerie () {
+    return null;
+}
+public boolean getEnUso () {
+    return false;
+}
+public void apagar () {
+}
+}
---
+++ clasesModelo/Salon.java
@@ -0,0 +1,14 @@
+package clasesModelo;
+
+public class Salon {
+private int numero;
+private int capacidad;
+private boolean tieneTele;
+
+public Salon (int numero, int capacidad)  {
+ 
+}
+  public void apartarSalon (String nombre) {
+    
+  } 
+}
