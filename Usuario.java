24f862921663dca30bd64a90268f0800a1fc94e2
Dulce Granados <dulcegranados.chun06@gmail.com>
2025-08-26 08:32:39 -0600

    Primer commit

---
+++ Usuario.java
@@ -0,0 +1,18 @@
+public class Usuario{
+    private String nombre; 
+    private String email; 
+    private int carnet; 
+
+    public void Usuario(String nombre, String email, int carnet){
+
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String verLibrosApartados(libro Libro){
+        return libros; 
+    }
+    public boolean tieneLibros(){
+        return true; 
+    }
+}
